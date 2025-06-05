75c4c83ca405e2c5728c909015f12b5c432fb3b9
simonelupone <simone.lupone@gmail.com>
2025-06-05 22:26:16 +0200

    feat: add style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from "react-router";
 
 const Header = () => {
   return (
-    <nav>
+    <nav className="h-24 bg-sky-500 text-white font-semibold text-xl flex items-center pl-4">
       <div className="flex gap-4">
         <NavLink to="/">Movies</NavLink>
         <NavLink to="/details">Movie details</NavLink>
